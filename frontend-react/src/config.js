9f65085b53707ac45470f2f5613ec9c92b8ca2c9
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-08 21:39:05 +0800

    added config.js

---
+++ frontend-react/src/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+  PRODUCT_API: import.meta.env.VITE_PRODUCT_API,
+  CART_API: import.meta.env.VITE_CART_API,
+  CHECKOUT_API: import.meta.env.VITE_CHECKOUT_API,
+};
+
+export default CONFIG;
