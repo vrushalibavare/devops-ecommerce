3395c7a28db824fcf7e856f201130f379cef4692
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-20 21:03:33 +0800

    deploy

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -66,7 +66,7 @@ resource "aws_lb_listener" "http" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.frontend.arn
   }
-  
+
   lifecycle {
     ignore_changes = [default_action]
   }
@@ -78,7 +78,7 @@ resource "aws_lb_target_group" "frontend" {
   protocol    = "HTTP"
   vpc_id      = module.vpc.vpc_id
   target_type = "ip"
-  
+
   lifecycle {
     create_before_destroy = true
   }
