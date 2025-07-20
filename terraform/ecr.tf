453bd946e74505fe8fc0da77a61a954e6baa5834
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-20 22:48:36 +0800

    change

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -13,6 +13,7 @@ resource "aws_ecr_repository" "repositories" {
 
   name                 = "${var.project_name}-${each.key}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
